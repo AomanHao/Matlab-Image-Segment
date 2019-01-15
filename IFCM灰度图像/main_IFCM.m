da2e1eecedf0889b6667110e2264e87e2301b730
AomanHao <1014325364@qq.com>
2019-01-15 19:33:00 +0800

    update  IFCM

--- IFCM灰度图像/main_IFCM.m
+++ IFCM灰度图像/main_IFCM.m
@@ -2,29 +2,36 @@ clear
 close all
 clc
 %% %%%%%%%%%%%%%%%图像%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
  I=imread('3096.jpg');
 
 if size(I,3) == 3
    I=rgb2gray(I);
 else
 end
-I=im2double(I);
-figure;imshow(I);title('(a)原始图像');imwrite(I,'1.jpg');
+figure;imshow(I);title('(a)原始图像');imwrite(I,'1.tiff','tiff','Resolution',300);%保存为tif
 %I=I;%不加噪声
 % I=imnoise(I,'speckle',deta_2);
-% I=imnoise(I,'salt & pepper',0.05); %加噪图
+I=imnoise(I,'salt & pepper',0.05); %加噪图椒盐噪声
 % I=imnoise(I,'gaussian',0,0.01); % 加高斯噪声
-%混合噪声
-%I1=imnoise(I,'salt & pepper',0.01);
-%I =imnoise(I1,'gaussian',0,0.01);
-figure;imshow(I);title('(b)加噪图像');imwrite(I,'2.jpg');
+
+ 
+figure;imshow(I);title('(b)加噪图像');imwrite(I,['2.',num2str(noise),'.tiff'],'tiff','Resolution',300);%保存为tif
 [m,n]=size(I);
 
-k=3;%聚类中心数目
+% k=4;
+k=2;
+r=3;
+m_index=2;
+beta=6;
+neighbor=3;%3*3邻域去噪
+lamda_s_enfcm=3;%s参数
 
+I=im2double(I);
 I4 = I(:);  %% 将图像灰度按列排列
+
 %% ------------------------ ifcm------------------------
-ifcm__label=zeros(m*n,1);
+ifcm_label=zeros(m*n,1);
 t=cputime;
 tic;
 [O2, U2, obj_fcn2] = ifcm(I4, k);
@@ -33,10 +40,10 @@ time_fcm_spatial_mean=cputime-t;
 maxU2 = max(U2);   %隶属度最大  
 for j=1:k
     index = find(U2(j, :) == maxU2);  %隶属度最大对应的像素位置
-    ifcm__label(index) = j;    
+   ifcm_label(index) = j;    
 end
-labels2=reshape(ifcm__label,[m n]);
-labels2=uint16(labels2);
+labels2=reshape(ifcm_label,[m n]);
+
 
 %显示聚类分割图
 labels2(find(labels2==1))=0;
@@ -45,4 +52,9 @@ labels2(find(labels2==3))=125;
 labels2(find(labels2==4))=180;
 labels2=uint8(labels2);
 % figure;imshow(labels2,[]);title('(c)聚类分割图');imwrite(labels2,'3.jpg');imwrite(labels2,'33.png')%保存为jpg
-figure;imshow(labels2,[]);title('(c)聚类分割图');imwrite(labels2,'3.tiff','tiff','Resolution',300);%保存为tif
+figure;imshow(labels2,[]);title('(c)聚类分割图');
+imwrite(labels2,['3.1.','-',noise_type,'.tiff'],'tiff','Resolution',300);%保存为tif
+
+
+
+end
